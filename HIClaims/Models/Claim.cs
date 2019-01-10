50d1a16bab732fc3725335271da28ef8bc08a53d
HITeam06 <HITeam06@HIHackTeam06>
2019-01-10 18:56:35 +0000

    Added "please" before all validation custom messages

--- HIClaims/Models/Claim.cs
+++ HIClaims/Models/Claim.cs
@@ -11,14 +11,14 @@ namespace HIClaims.Models
         [Required(ErrorMessage ="Please Enter a valid Claim Number")] //* code change by Team6 *//
         [Range(0, int.MaxValue, ErrorMessage = "Please enter valid Claim Number")]
         public int ClaimNo { get; set; }
-        [Required(ErrorMessage = "Enter a valid Customer Name")]
+        [Required(ErrorMessage = "Please Enter a valid Customer Name")]
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Name Should only contain alphabets")]
         public string CustomerName { get; set; }
-        [Required(ErrorMessage = "Enter a valid Claim Date")]
+        [Required(ErrorMessage = "Please Enter a valid Claim Date")]
         public DateTime ClaimedDate { get; set; }
-        [Required(ErrorMessage = "Enter a valid Claim Amount")]
+        [Required(ErrorMessage = "Please Enter a valid Claim Amount")]
         public float ClaimAmount { get; set; }
-        [Required(ErrorMessage = "Enter a valid Policy Number")]
+        [Required(ErrorMessage = "Please Enter a valid Policy Number")]
         public int PolicyNo { get; set;}
         public string Gender { get; set; }
         public string Description { get; set; }
